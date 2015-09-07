b16b1c2850a462918bc8fa2acf2ea1160d3d1071
Andriy Lukashchuk <darksundarksun@gmail.com>
2015-09-07 12:07:23 +0300

    db config

---
+++ config/deploy/production.rb
@@ -0,0 +1,14 @@
+# Simple Role Syntax
+# ==================
+# Supports bulk-adding hosts to roles, the primary server in each group
+# is considered to be the first unless any hosts have the primary
+# property set.  Don't declare `role :all`, it's a meta role.
+
+server '146.185.168.46', user: 'deploy', roles: %w{web app db}
+
+set :rails_env, 'production'
+
+Capistrano::Env.use do |env|
+  env.add 'RAILS_ENV', 'production'
+  env.formatter = :dotenv
+end
