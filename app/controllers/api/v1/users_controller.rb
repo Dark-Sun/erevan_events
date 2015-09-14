a498609d85aa638ebd423d82b1b7ffc4c5df74f4
Andriy Lukashchuk <darksundarksun@gmail.com>
2015-09-14 14:50:04 +0300

    bugfix

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -18,7 +18,7 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.permit(:email, :phone, :name)
+    params.permit(:email, :phone, :name, :password)
   end
 
   def login_params
