ce2f579700668325304ce0c05d0c7133220a8b78
Andriy Lukashchuk <darksundarksun@gmail.com>
2015-09-28 12:18:49 +0300

    translations for push messages

--- app/admin/message.rb
+++ app/admin/message.rb
@@ -2,11 +2,16 @@ ActiveAdmin.register_page 'Message' do
 
   page_action :send_message, method: :put do
     if params[:user_category] == ""
-      UserCategory.send_gcm_to_all(params[:message])
+      UserCategory.send_gcm_to_all(params[:message], 
+                                   message_arm: params[:message_arm], 
+                                   message_ru:  params[:message_ru])
       redirect_to :back, notice: "Message sent"
     else
       category = UserCategory.find(params[:user_category])
-      category.send_gcm(params[:message], params[:user_category])
+      category.send_gcm(params[:message], 
+                        message_arm: params[:message_arm], 
+                        message_ru:  params[:message_ru],
+                        category:    params[:user_category])
       redirect_to :back, notice: "Message sent"
     end
   end
--- app/models/user_category.rb
+++ app/models/user_category.rb
@@ -7,22 +7,24 @@ class UserCategory < ActiveRecord::Base
   has_many :user_user_categories
   has_many :users, through: :user_user_categories
 
-  def self.send_gcm_to_all(message)
+  def self.send_gcm_to_all(message, message_arm: message_arm, message_ru: message_ru)
     gcm = GCM.new("AIzaSyCChwudE2ZPtMvDaeCpaKSXsiZnvQh_6uc")
     User.all.each do |user|
       registration_ids = [user.gcm_id]
-      p "sending #{message}"
-      options = {data: {category: "all", message: "#{message}"}, collapse_key: "updated_score"}
+      options = { data: {category: "all", message: "#{message}", message_arm: "#{message_arm}", 
+                  message_ru: "#{message_ru}"}, collapse_key: "updated_score" }
+      p "sending #{options}"    
       response = gcm.send(registration_ids, options)
     end
   end
 
-  def send_gcm(message, category)
+  def send_gcm(message, message_arm: message_arm, message_ru: message_ru, category: category)
     gcm = GCM.new("AIzaSyCChwudE2ZPtMvDaeCpaKSXsiZnvQh_6uc")
     self.users.each do |user|
       registration_ids = [user.gcm_id]
-      p "sending #{message}"
-      options = {data: {category: "#{category}", message: "#{message}"}, collapse_key: "updated_score"}
+      options = { data: {category: "all", message: "#{message}", message_arm: "#{message_arm}", 
+                  message_ru: "#{message_ru}"}, collapse_key: "updated_score" }
+      p "sending #{options}"    
       response = gcm.send(registration_ids, options)
     end
   end
