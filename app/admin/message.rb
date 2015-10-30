a67696500eef5ed39fadf6049524a5def5529c59
Andriy Lukashchuk <darksundarksun@gmail.com>
2015-10-30 14:41:03 +0200

    push msgs fix2

--- app/admin/message.rb
+++ app/admin/message.rb
@@ -4,17 +4,25 @@ ActiveAdmin.register_page 'Message' do
     p "======="
     p params
 
+    user_ids = []
     UserUserCategory.where(user_category_id: params[:categories]).each do |user_usercategory|
       next unless user_usercategory.user
-      user_usercategory.user.send_gcm(params[:message], 
+      user_ids << user_usercategory.user.id 
+    end
+
+    user_ids.uniq.each do |user_id|
+      user = User.find(user_id)
+      next unless user
+      user.send_gcm(params[:message], 
                         message_arm: params[:message_arm], 
                         message_ru:  params[:message_ru],
                         category:    params[:user_category])
-      user_usercategory.user.send_apns(params[:message], 
+      user.send_apns(params[:message], 
                         message_arm: params[:message_arm], 
                         message_ru:  params[:message_ru],
                         category:    params[:user_category])
     end
+
 
     redirect_to :back, notice: "Message sent"
   end
