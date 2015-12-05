504d4fd984bf606f707d8440a65950e7c26acf8f
Andriy Lukashchuk <darksundarksun@gmail.com>
2015-12-05 12:51:12 +0200

    added eager loading

--- app/controllers/api/v1/venue_categories_controller.rb
+++ app/controllers/api/v1/venue_categories_controller.rb
@@ -4,9 +4,9 @@ class Api::V1::VenueCategoriesController < ApplicationController
 
   def index
     @venue_categories = VenueCategory.with_deleted
-    @venues           = Venue.with_deleted
-    @events           = Event.with_deleted.upcoming
-    @venue_photos     = VenuePhoto.all
+    @venues           = Venue.with_deleted.includes(:venue_category)
+    @events           = Event.with_deleted.upcoming.includes(:venue)
+    @venue_photos     = VenuePhoto.all.includes(:venue)
     @user_categories  = UserCategory.all
   end
 
